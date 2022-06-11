074cf87c37d0824a528c480d2ae42e0ed0955f7c
Nishant <nishantjambhulkar6@gmail.com>
2022-06-11 22:32:09 +0530

    Assigment/middle part1

---
+++ src/controllers/middlecontroller.js
@@ -0,0 +1,5 @@
+const router = require("../routes/route")
+const Router=function(req,res){
+    console.log("This is Api")
+}
+module.exports.Router=Router
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const controller = require("../controllers/middlecontroller")
 const commonMW = require ("../middlewares/commonMiddlewares")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+router.get("/test-me", commonMW.mid1, controller.Router)
 
-
-
-
-router.post("/createBook", BookController.createBook  )
 
 
 
@@ -45,16 +38,6 @@ router.post("/createBook", BookController.createBook  )
 // router.get('/register',  UserController.register)
 
 
-
-
-
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
-
-
-
-// router.get("/basicRoute2", commonMW.mid1, UserController.basicCode2)
-// router.get("/basicRoute3", commonMW.mid2, UserController.basicCode3)
-// router.get("/basicRoute4", commonMW.mid1, commonMW.mid4, UserController.basicCode4)
 
 
 
